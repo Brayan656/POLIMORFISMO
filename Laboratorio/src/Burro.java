e50851b8e2c5d1b85d4fb0911d5f022f309f978f
Brayan656 <55366835+Brayan656@users.noreply.github.com>
2019-09-16 00:07:17 -0500

    Update Burro.java

--- Laboratorio/src/Burro.java
+++ Laboratorio/src/Burro.java
@@ -1,7 +1,7 @@
 
 public class Burro implements PuedeCantar { 
 	    public void cantar() { 
-	        System.out.println("Beeeeee"); 
+	        System.out.println("hiiihoooooo"); 
 	    } 
 	} 
     
